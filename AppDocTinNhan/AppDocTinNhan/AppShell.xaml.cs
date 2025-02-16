c6f79b9bb35143ea941749969e773c5fc1b17164
nguyenluongk2k4 <nguyenluongk2k4@gmail.com>
2025-02-16 15:49:54 +0700

    update

---
+++ AppDocTinNhan/AppDocTinNhan/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppDocTinNhan
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppDocTinNhan/AppDocTinNhan/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace AppDocTinNhan
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSpeakButtonClicked(object sender, EventArgs e)
+        {
+            await SpeakText("Xin chào! Đây là giọng đọc của .NET MAUI.");
+        }
+
+        private async Task SpeakText(string text)
+        {
+            // Lấy MainActivity từ Application Context
+            var activity = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity as MainActivity;
+            activity?.AcquireWakeLock(); // Giữ máy hoạt động
+
+            await TextToSpeech.Default.SpeakAsync(text);
+
+            activity?.ReleaseWakeLock(); // Thả Wake Lock khi đọc xong
+        }
+
+    }
+
+}
